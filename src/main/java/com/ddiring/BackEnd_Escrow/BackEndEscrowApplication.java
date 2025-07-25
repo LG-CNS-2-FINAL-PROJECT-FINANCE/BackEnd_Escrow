b1d56147979ffd6671c27126cf4dfb704e5167cf
5ohmydays5 <ooojjhn5@gmail.com>
2025-07-25 13:58:22 +0900

    Initialize

---
+++ src/main/java/com/ddiring/BackEnd_Escrow/BackEndEscrowApplication.java
@@ -0,0 +1,13 @@
+package com.ddiring.BackEnd_Escrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEscrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndEscrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddiring/BackEnd_Escrow/BackEndEscrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiring.BackEnd_Escrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEscrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
