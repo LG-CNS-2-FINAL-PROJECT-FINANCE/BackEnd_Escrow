42380a49afb5984efbc1a82e76b1339b44307e21
5ohmydays5 <ooojjhn5@gmail.com>
2025-09-03 13:58:39 +0900

    Refactor: notification test

--- src/main/java/com/ddiring/BackEnd_Escrow/service/EscrowService.java
+++ src/main/java/com/ddiring/BackEnd_Escrow/service/EscrowService.java
@@ -54,14 +54,14 @@ public class EscrowService {
         escrowRepository.save(escrow);
 
         //test
-        notificationProducer.sendNotification(
-                //List.of("1", "2", "3", "4", "5"),
-                //List.of("21be0bd8-f9f0-4d05-b385-4cb419d553a4"),
-                List.of("21be0bd8-f9f0-4d05-b385"),
-                NotificationType.INFORMATION.name(),
-                "에스크로 계좌 생성",
-                "에스크로 계좌가 생성되었습니다.: " + accountNumber
-        );
+//        notificationProducer.sendNotification(
+//                //List.of("1", "2", "3", "4", "5"),
+//                //List.of("21be0bd8-f9f0-4d05-b385-4cb419d553a4"),
+//                List.of("21be0bd8-f9f0-4d05-b385"),
+//                NotificationType.INFORMATION.name(),
+//                "에스크로 계좌 생성",
+//                "에스크로 계좌가 생성되었습니다.: " + accountNumber
+//        );
 
         return CreateAccountResponse.fromEntity(escrow);
     }
